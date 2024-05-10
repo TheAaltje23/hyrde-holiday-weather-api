b575fdc6c05f1a87a4c22e653598586e7a12e7ab
TheAaltje23 <a.van.noordenne@hotmail.com>
2024-05-10 15:31:31 +0200

    initial layout + indexpage

---
+++ hyrde.challenge.ui/src/layouts/LayoutWeatherApp.vue
@@ -0,0 +1,18 @@
+<template>
+    <q-layout view="hHh lpr fff">
+        <q-header reveal elevated class="bg-primary text-white">
+            <q-toolbar>
+                <q-toolbar-title>
+                    <q-avatar>
+                        <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+                    </q-avatar>
+                    Hyrde Holiday Weather App
+                </q-toolbar-title>
+            </q-toolbar>
+        </q-header>
+
+        <q-page-container>
+            <router-view />
+        </q-page-container>
+    </q-layout>
+</template>
---
+++ hyrde.challenge.ui/src/pages/IndexWeatherApp.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="q-pa-md input-container">
+    <div class="q-gutter-y-md column" style="max-width: 300px">
+      <q-input outlined bottom-slots v-model="text" label="Enter a city name">
+        <template v-slot:prepend>
+          <q-icon name="place" />
+        </template>
+        <template v-slot:append>
+          <q-icon name="close" @click="text = ''" class="cursor-pointer" />
+        </template>
+      </q-input>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      text: ref(''),
+      ph: ref('')
+    }
+  }
+}
+</script>
