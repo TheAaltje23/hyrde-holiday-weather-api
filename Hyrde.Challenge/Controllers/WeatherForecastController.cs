a511c07ba4589cd44d7c0dd5fca17a1e97a4a7a7
TheAaltje23 <a.van.noordenne@hotmail.com>
2024-05-12 23:42:01 +0200

    controller

--- Hyrde.Challenge/Controllers/WeatherForecastController.cs
+++ Hyrde.Challenge/Controllers/WeatherForecastController.cs
@@ -1,5 +1,8 @@
 using Hyrde.Challenge.Models;
 using Microsoft.AspNetCore.Mvc;
+using Hyrde.Challenge.Dto;
+using Microsoft.Extensions.Primitives;
+using Hyrde.Challenge.Services;
 
 namespace Hyrde.Challenge.Controllers
 {
@@ -9,21 +12,29 @@ namespace Hyrde.Challenge.Controllers
     {
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        private readonly IWeatherService _service;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherService service)
         {
             _logger = logger;
+            _service = service;
         }
 
         [HttpGet("GetToday")]
-        public Weather GetToday()
+        public async Task<ResponseDto> GetToday(string cityName)
         {
-            // Replace this implementation with your own.
-            return new Weather()
+            if (string.IsNullOrEmpty(cityName))
             {
-                City = "Maarssen",
-                Country = "The Netherlands",
-                TempFahrenheit = 55.4f
-            };
+                return new ResponseDto(success: false, data: null, errors: ["City name is required."], validationMessage: "City name is required.");
+            }
+
+            Weather weather = await _service.Today(cityName);
+            if (weather == null)
+            {
+                return new ResponseDto(success: false, data: null, errors: new List<string> { "City not found." }, validationMessage: "City not found.");
+            }
+
+            return new ResponseDto(success: true, data: weather, errors: null, validationMessage: "Weather information retrieved successfully.");
         }
 
         [HttpGet("GetForecast")]
@@ -31,21 +42,18 @@ namespace Hyrde.Challenge.Controllers
         {
             // Replace this implementation with your own.
             return new List<Weather>() {
-                new Weather() 
+                new Weather()
                 {
                     City = "Maarssen",
                     Country = "The Netherlands",
-                    TempFahrenheit = 55.4f
                 },
                 new Weather() {
                     City = "Maarssen",
                     Country = "The Netherlands",
-                    TempFahrenheit = 58.4f
                 },
                 new Weather() {
                     City = "Maarssen",
                     Country = "The Netherlands",
-                    TempFahrenheit = 61.4f
                 }
             };
         }
