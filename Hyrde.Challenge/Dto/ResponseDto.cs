450efbd6dab44c30f2d3d0ba2dcdfd21421e86a6
TheAaltje23 <a.van.noordenne@hotmail.com>
2024-05-12 23:42:20 +0200

    Dtos that I might use

---
+++ Hyrde.Challenge/Dto/ResponseDto.cs
@@ -0,0 +1,20 @@
+namespace Hyrde.Challenge.Dto
+{
+    public class ResponseDto
+    {
+
+        public bool Success { get; set; }
+        public object Data { get; set; }
+        public List<string> Errors { get; set; }
+        public string ValidationMessage { get; set; }
+
+        public ResponseDto(bool success, object data, List<string> errors, string validationMessage)
+        {
+            Success = success;
+            Data = data;
+            Errors = errors;
+            ValidationMessage = validationMessage;
+        }
+
+    }
+}
