77c4003c47cef3ebcc22f01867825aec1ffb57af
TheAaltje23 <a.van.noordenne@hotmail.com>
2024-05-15 17:21:26 +0200

    forecast frontend

--- hyrde.challenge.ui/src/pages/IndexWeatherApp.vue
+++ hyrde.challenge.ui/src/pages/IndexWeatherApp.vue
@@ -2,7 +2,7 @@
   <div class="q-pa-md page-container">
     <div id="input-btn-container">
       <div class="q-gutter-y-md column input-container" style="max-width: 300px">
-        <q-input outlined v-model="text" bg-color="white" label="Enter a location" @keyup.enter="fetchData">
+        <q-input outlined v-model="text" bg-color="white" label="Enter a location" @keyup.enter="fetchDataToday">
           <template v-slot:prepend>
             <q-icon name="place" />
           </template>
@@ -11,14 +11,13 @@
           </template>
         </q-input>
       </div>
-      <q-btn id="search-btn" @click="fetchData" color="primary" label="Search" />
+      <q-btn id="search-btn" @click="fetchDataToday" color="primary" label="Search" />
     </div>
     <div id="toggle-container" v-if="weatherToday">
       <q-btn-toggle
         v-model="model"
-        class="my-custom-toggle"
+        class="today-forecast-toggle"
         no-caps
-        rounded
         unelevated
         toggle-color="primary"
         color="positive"
@@ -27,9 +26,10 @@
           {label: 'Today', value: 'today'},
           {label: 'Forecast', value: 'forecast'}
         ]"
+        @update:model-value="handleToggle"
       />
     </div>
-    <div id="output-wrapper" v-if="weatherToday">
+    <div id="output-wrapper-today" v-if="weatherToday && model === 'today'">
       <div id="location-wrapper">
         <h2>{{weatherToday.data["city"]}}, {{weatherToday.data["region"]}}, {{weatherToday.data["country"]}}</h2>
       </div>
@@ -37,7 +37,7 @@
         <h3>{{weatherToday.data["conditionText"]}}</h3>
       </div>
       <div id="icon-temp-wrapper">
-        <div><img id="weatherIcon" :src="weatherToday.data['conditionIcon']" alt="Weather Icon"></div>
+        <div class="weather-icon"><img :src="weatherToday.data['conditionIcon']" alt="Weather Icon"></div>
         <div>{{weatherToday.data["tempCelcius"]}}°C</div>
       </div>
       <div id="info-wrapper">
@@ -45,8 +45,8 @@
           <div class="info-row">
             <div><q-icon class="info-icon" color="primary" name="air" /></div>
             <div>
-              <div id="info-title">Wind</div>
-              <div id="info-data">{{weatherToday.data["windKph"]}}km/h</div>
+              <div class="info-title">Wind</div>
+              <div class="info-data">{{weatherToday.data["windKph"]}}km/h</div>
             </div>
           </div>
         </div>
@@ -54,8 +54,8 @@
           <div class="info-row">
             <div><q-icon class="info-icon" color="primary" name="explore" /></div>
             <div>
-              <div id="info-title">Direction</div>
-              <div id="info-data">{{weatherToday.data["windDir"]}}</div>
+              <div class="info-title">Direction</div>
+              <div class="info-data">{{weatherToday.data["windDir"]}}</div>
             </div>
           </div>
         </div>
@@ -63,8 +63,8 @@
           <div class="info-row">
             <div><q-icon class="info-icon" color="primary" name="water_drop" /></div>
             <div>
-              <div id="info-title">Precipitation</div>
-              <div id="info-data">{{weatherToday.data["precipitationMm"]}}mm</div>
+              <div class="info-title">Precipitation</div>
+              <div class="info-data">{{weatherToday.data["precipitationMm"]}}mm</div>
             </div>
           </div>
         </div>
@@ -72,10 +72,20 @@
           <div class="info-row">
             <div><q-icon class="info-icon" color="primary" name="water" /></div>
             <div>
-              <div id="info-title">Humidity</div>
-              <div id="info-data">{{weatherToday.data["humidity"]}}%</div>
+              <div class="info-title">Humidity</div>
+              <div class="info-data">{{weatherToday.data["humidity"]}}%</div>
             </div>
           </div>
+        </div>
+      </div>
+    </div>
+    <div id="output-wrapper-forecast" v-if="weatherForecast && model === 'forecast'">
+      <div id="forecast-items" v-for="(forecastItem, index) in weatherForecast.data" :key="index">
+        <div class="forecast-day">{{forecastItem["forecastDate"]}}</div>
+        <div class="weather-icon"><img :src="forecastItem['conditionIcon']" alt="Weather Icon"></div>
+        <div class="forecast-temp">
+          <div class="forecast-temp-max">{{forecastItem["maxTempCelcius"]}}°C</div>
+          <div class="forecast-temp-min">{{forecastItem["minTempCelcius"]}}°C</div>
         </div>
       </div>
     </div>
@@ -90,8 +100,10 @@ export default {
   setup () {
     const text = ref('')
     const weatherToday = ref(null)
+    const weatherForecast = ref(null)
+    const model = ref('today')
 
-    const fetchData = async () => {
+    const fetchDataToday = async () => {
       try {
         const response = await axios.get(`http://localhost:5114/WeatherForecast/GetToday?query=${text.value}`)
         weatherToday.value = response.data
@@ -101,15 +113,39 @@ export default {
       }
     }
 
+    const fetchDataForecast = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5114/WeatherForecast/GetForecast?query=${text.value}`)
+        weatherForecast.value = response.data
+        console.log(response.data)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    const handleToggle = () => {
+      if (model.value === 'today') {
+        fetchDataToday()
+      }
+      if (model.value === 'forecast') {
+        fetchDataForecast()
+      }
+    }
+
     const clearText = () => {
       text.value = ''
       weatherToday.value = null
+      weatherForecast.value = null
     }
 
     return {
       text,
       weatherToday,
-      fetchData,
+      weatherForecast,
+      model,
+      fetchDataToday,
+      fetchDataForecast,
+      handleToggle,
       clearText
     }
   }
