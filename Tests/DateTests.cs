124bdb19bafad2370707b27b5fede583705be52c
TheAaltje23 <a.van.noordenne@hotmail.com>
2024-06-04 09:47:36 +0200

    Setup unit tests

---
+++ Tests/DateTests.cs
@@ -0,0 +1,16 @@
+using Hyrde.Challenge.Services
+
+namespace Tests
+{
+    [TestClass]
+    public class DateTests
+    {
+        [TestMethod]
+        public void TestMethod1(string date)
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+}
