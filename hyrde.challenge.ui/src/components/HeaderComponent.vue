8118c63e346a66e8a9140710b140f8853bcf10a8
TheAaltje23 <a.van.noordenne@hotmail.com>
2024-06-18 16:30:40 +0200

    drawer for login functionality

---
+++ hyrde.challenge.ui/src/components/HeaderComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <q-header elevated>
+        <q-toolbar>
+            <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
+            <q-toolbar-title><a class="header-title" href="#">Alex's Weather Forecast</a></q-toolbar-title>
+        </q-toolbar>
+    </q-header>
+    <q-drawer v-model="drawer" class="drawer">
+        <q-list padding class="menu-list drawer-list" >
+            <q-item clickable v-ripple>
+                <q-item-section>
+                    Login
+                </q-item-section>
+            </q-item>
+        </q-list>
+    </q-drawer>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  name: 'HeaderComponent',
+  setup () {
+    return {
+      drawer: ref(false)
+    }
+  }
+}
+</script>
--- hyrde.challenge.ui/src/layouts/LayoutWeatherApp.vue
+++ hyrde.challenge.ui/src/layouts/LayoutWeatherApp.vue
@@ -1,7 +1,17 @@
 <template>
-    <q-layout view="hhh lpr fff">
+    <q-layout view="hHh Lpr lff">
+        <HeaderComponent />
         <q-page-container>
             <router-view />
         </q-page-container>
     </q-layout>
 </template>
+
+<script>
+import HeaderComponent from 'src/components/HeaderComponent.vue'
+export default {
+  components: {
+    HeaderComponent
+  }
+}
+</script>
